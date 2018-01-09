5a4a4a97cdc6d927851a0dbdaa0fb80d049d3dc0
Admin <admin@Admins-MacBook-Pro.local>
2018-01-09 15:40:17 -0800

    Got basic style of navbar down. Have events for when tool btn is clicked and msg btn

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,3 +1,6 @@
-document.getElementById("tools-btn").addEventListener("click",function(){
+document.getElementById("nav-tools-btn").addEventListener("click",function(){
 	console.log("tools btn was clicked");	
+})
+document.getElementById("nav-msg-btn").addEventListener("click",function(){
+	console.log("Message btn was clicked");	
 })
