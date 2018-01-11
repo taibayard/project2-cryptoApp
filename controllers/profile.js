98ccebde13347f4b3db20bf54d809a04cf914e9e
Admin <admin@Admins-MacBook-Pro.local>
2018-01-11 10:00:46 -0800

    fixed style issues with footer and user view rendering

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,12 +8,23 @@ var userData = null;
 var profileView = 'profile/profileLayout.ejs';
 
 
+var tempUser = {
+	username:"BobisCool1231",
+	password:"t3902kdfjls039032",
+	email:"bobtest@test.com",
+	phone:"555-555-5555",
+	firstname:"Bob",
+	lastname:"Ross"
+}
+
 router.get('/dash', isLoggedIn, function(req, res) {
-    userData = res.locals.currentUser.dataValues;
-    delete userData.password;
-	res.render('profile/dash', {layout: profileView,user:userData});
+    // userData = res.locals.currentUser.dataValues;
+    // delete userData.password;
+	// res.render('profile/dash', {layout: profileView,user:userData});
+	res.render('profile/dash', {layout: profileView,user:tempUser});
 });
 router.get("/settings", isLoggedIn, function(res,res){
-	res.render('profile/settings', {layout: profileView,user:userData});
+	// res.render('profile/settings', {layout: profileView,user:userData});
+	res.render('profile/settings', {layout: profileView,user:tempUser});
 })
 module.exports = router;
--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next){
-  if(!req.user){
+  if(req.user){
     req.flash('error', 'You must be logged in to view this page');
     res.redirect('/auth/login');
   }
