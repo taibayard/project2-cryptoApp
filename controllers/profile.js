ac03c1260c5c4e8b2ff548318ca3eea0970fc8e0
Admin <admin@Admins-MacBook-Pro.local>
2018-01-13 04:57:56 -0800

    added delete route. can now delete wallet and all coins associated with that wallet

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,55 +11,55 @@ var userData = null;
 var profileView = 'profile/profileLayout.ejs';
 //ethplorer api
 let ethplorer = {
-    baseUrl: "https://api.ethplorer.io/",
-    key: "?apiKey=" + process.env.ethplorerKey,
-    getWallet: function(address) {
-        return new Promise(function(resolve, reject) {
-            request.get(ethplorer.baseUrl + "getAddressInfo/" + address + ethplorer.key, function(error, response, body) {
-                let parsedBody = JSON.parse(body);
-                if(parsedBody.ETH.balance>=0 || parsedBody.tokens){
-                	resolve(parsedBody);
-                }else{
-                	resolve(error);
+        baseUrl: "https://api.ethplorer.io/",
+        key: "?apiKey=" + process.env.ethplorerKey,
+        getWallet: function(address) {
+            return new Promise(function(resolve, reject) {
+                request.get(ethplorer.baseUrl + "getAddressInfo/" + address + ethplorer.key, function(error, response, body) {
+                    let parsedBody = JSON.parse(body);
+                    if (parsedBody.ETH.balance >= 0 || parsedBody.tokens) {
+                        resolve(parsedBody);
+                    } else {
+                        resolve(error);
+                    }
+                })
+            });
+        },
+        convertBalance: function(bal, dec) {
+            //test code
+            bal = bal.toString();
+            if (bal.indexOf("e") != -1) {
+                // handles numbers like 3.3e+37 or 2e+28
+                bal = bal.replace(".", "");
+                let eOffset = bal.substring(bal.indexOf("+") + 1, bal.length);
+                bal = bal.substring(0, bal.indexOf("e"));
+                eOffset = parseInt(eOffset) - 1; // -1 otherwise it will add one to many zeros 
+                while (eOffset > 0) {
+                    bal += "0";
+                    eOffset--;
                 }
-            })
-        });
-    },
-    convertBalance: function(bal, dec) {
-    //test code
-    bal = bal.toString();
-    if (bal.indexOf("e") != -1) {
-        // handles numbers like 3.3e+37 or 2e+28
-        bal = bal.replace(".", "");
-        let eOffset = bal.substring(bal.indexOf("+") + 1, bal.length);
-        bal = bal.substring(0, bal.indexOf("e"));
-        eOffset = parseInt(eOffset) - 1;// -1 otherwise it will add one to many zeros 
-        while (eOffset > 0) {
-            bal += "0";
-            eOffset--;
+                bal.replace(".", "");
+            }
+            let offset = bal.length - dec;
+            if (offset > 0) {
+                // coin balance is greater than 0 with a decimal 
+                let temp = bal.split("");
+                temp.splice(offset, 0, ".");
+                bal = temp.join("");
+            } else {
+                //coin balance is less than 0 so it is a decimal
+                offset *= -1;
+                while (offset > 0) {
+                    bal = "0" + bal;
+                    offset--;
+                }
+                bal = "0." + bal;
+            }
+            return parseFloat(bal);
         }
-        bal.replace(".", "");
-    }
-    let offset = bal.length - dec;
-    if (offset > 0) {
-        // coin balance is greater than 0 with a decimal 
-        let temp = bal.split("");
-        temp.splice(offset,0,".");
-        bal = temp.join("");
-    } else {
-        //coin balance is less than 0 so it is a decimal
-        offset *= -1;
-        while (offset > 0) {
-            bal = "0" + bal;
-            offset--;
-        }
-        bal = "0." + bal;
-    }
-    return parseFloat(bal);
-}
 
-}
-//get routes for pages.
+    }
+    //get routes for pages.
 router.get('/dash', isLoggedIn, function(req, res) {
     userData = res.locals.currentUser.dataValues;
     delete userData.password;
@@ -74,19 +74,20 @@ router.get("/settings", isLoggedIn, function(req, res) {
             userId: userData.id
         }
     }).then(function(wallets) {
-	    res.render('profile/settings', {
-	        layout: profileView,
-	        user: userData,
-	        wallet : wallets
-	    })
-	    res.send(wallets);
-	}).catch(function(err) {
+        res.render('profile/settings', {
+            layout: profileView,
+            user: userData,
+            wallet: wallets
+        })
+    }).catch(function(err) {
         //no wallets exist?
         res.send("error finding existing wallets");
     })
 })
 
 //settings posts
+
+//creates wallet in db
 router.post("/settings/wallet", isLoggedIn, function(req, res) {
     //pulling params from body
     let type = req.body.type.toLowerCase();
@@ -95,66 +96,66 @@ router.post("/settings/wallet", isLoggedIn, function(req, res) {
     switch (type) {
         case "token":
         case "ethereum":
-        	type="ethereum/token";
+            type = "ethereum/token";
             //handle request to ethereum based walllet
             let walletRequest = ethplorer.getWallet(address);
             //runs at end of promise (when request is done)
             walletRequest.then(function(walletData) {
-            	if(!walletData.tokens){
-            		if(walletData.ETH.balance > 0){
-            			//do something with ether coins
-            		}else{
-            			//no coins in wallet
-            			res.send("No coins in wallet");
-            		}
-            	}
-                for(let i =0;i<walletData.tokens.length;i++){
-                	walletData.tokens[i].balance = ethplorer.convertBalance(walletData.tokens[i].balance,walletData.tokens[i].tokenInfo.decimals)
+                if (!walletData.tokens) {
+                    if (walletData.ETH.balance > 0) {
+                        //do something with ether coins
+                    } else {
+                        //no coins in wallet
+                        res.send("No coins in wallet");
+                    }
+                }
+                for (let i = 0; i < walletData.tokens.length; i++) {
+                    walletData.tokens[i].balance = ethplorer.convertBalance(walletData.tokens[i].balance, walletData.tokens[i].tokenInfo.decimals)
                 }
                 return walletData;
-            }).then(function(result){
-            	//time to create wallet and add coins
-	            db.wallet.findOrCreate({
-	                where: {
-	                    address: address.toLowerCase(),
-	                	userId : userData.id
-	                },
-	                defaults: {
-	                    type: type
-	                }
-	            }).spread(function(wallet, wasCreated) {
-	                if (wasCreated) {
-	                	//was no duplicate add coins to database
-	                	//add ETH if balance > 0 
-	                	if(result.ETH.balance > 0){
-	                        db.coin.create({
-	                            walletId: wallet.id,
-	                            name: "ETH",
-	                            owned: result.ETH.balance
-	                        });
-	                	}
-	                	//add tokens
-	                	result.tokens.forEach(function(coin){
-	                        db.coin.create({
-	                        	walletId:wallet.id,
-	                        	name:coin.tokenInfo.symbol,
-	                        	owned:coin.balance
-	                        });
-	                	});
-	                } else {
-	                    //Contains duplicate ( fail )
-	                    res.send("wallet already exists")
-	                }
-	            }).catch(function(err) {
-	                res.send("Error adding wallet");
-	            });
+            }).then(function(result) {
+                //time to create wallet and add coins
+                db.wallet.findOrCreate({
+                    where: {
+                        address: address.toLowerCase(),
+                        userId: userData.id
+                    },
+                    defaults: {
+                        type: type
+                    }
+                }).spread(function(wallet, wasCreated) {
+                    if (wasCreated) {
+                        //was no duplicate add coins to database
+                        //add ETH if balance > 0 
+                        if (result.ETH.balance > 0) {
+                            db.coin.create({
+                                walletId: wallet.id,
+                                name: "ETH",
+                                owned: result.ETH.balance
+                            });
+                        }
+                        //add tokens
+                        result.tokens.forEach(function(coin) {
+                            db.coin.create({
+                                walletId: wallet.id,
+                                name: coin.tokenInfo.symbol,
+                                owned: coin.balance
+                            });
+                        });
+                    } else {
+                        //Contains duplicate ( fail )
+                        res.send("wallet already exists")
+                    }
+                }).catch(function(err) {
+                    res.send("Error adding wallet");
+                });
 
-            	//going back to settings
-	            res.redirect('/profile/settings');
+                //going back to settings
+                res.redirect('/profile/settings');
             }).catch(function(err) {
                 res.send("Error getting wallet");
             });
-                // res.redirect("/settings");
+            // res.redirect("/settings");
             break;
         case "bitcoin":
             //handle request to bitcoin based wallet
@@ -163,7 +164,41 @@ router.post("/settings/wallet", isLoggedIn, function(req, res) {
             break;
         default:
             res.send("ERROR INVALID WALLET TYPE");
-        break;
+            break;
     }
+});
+
+//delete routes
+
+//deletes wallet from database
+router.delete("/settings/:address", function(req, res) {
+    //finding users wallet
+    db.wallet.findOne({
+        where: {
+            address: req.params.address,
+            userId: userData.id
+        }
+    }).then(function(wallet) {
+    	//found wallet
+        db.coin.destroy({
+            where: {
+                walletId: wallet.id
+            }
+        }).then(function(result) {
+        	//deleted coins
+            db.wallet.destroy({
+                where: {
+                    address: req.params.address,
+                    userId: userData.id
+                }
+            }).then(function(deleted) {
+            	//deleted wallet
+                res.send("success");
+            });
+        });
+    }).catch(function(err) {
+    	console.log("was error finding wallet in db");
+       	res.send("was error finding wallet in db");
+    });
 });
 module.exports = router;
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,39 +1,43 @@
 //function to be fired when a wallet drop down item has been clicked
-function selectedWalletType(e){
-	try{
-		//removing previous selection id
-		document.getElementById("selectedWalletDropdown").removeAttribute("id");
-		e.setAttribute("id","selectedWalletDropdown");
-	}catch(err){
-		//was no previous item selected set item
-		e.setAttribute("id","selectedWalletDropdown")
-	}
-	document.getElementById("walletTypeInput").value = e.innerText;
+function selectedWalletType(e) {
+    try {
+        //removing previous selection id
+        document.getElementById("selectedWalletDropdown").removeAttribute("id");
+        e.setAttribute("id", "selectedWalletDropdown");
+    } catch (err) {
+        //was no previous item selected set item
+        e.setAttribute("id", "selectedWalletDropdown")
+    }
+    document.getElementById("walletTypeInput").value = e.innerText;
 }
 
 //IFFE
 
 //settings nav innertext when user visits settings page
-(function(){
-	document.getElementById("nav-name").innerText = "Settings";
+(function() {
+    document.getElementById("nav-name").innerText = "Settings";
 })();
 //adding event listeners for wallet type drop down items
-(function(){
-	let x = document.getElementsByClassName("wallet-dropdown-item");
-	for(let i =0;i<x.length;i++){
-		x[i].addEventListener("click",function(){
-			console.log(x[i],"Was clicked");
-			selectedWalletType(this);
-		})
-	}
+(function() {
+    let x = document.getElementsByClassName("wallet-dropdown-item");
+    for (let i = 0; i < x.length; i++) {
+        x[i].addEventListener("click", function() {
+            console.log(x[i], "Was clicked");
+            selectedWalletType(this);
+        })
+    }
 })();
 //adding event listeners for wallete delete buttons
-(function(){
-	let x = document.getElementsByClassName("wallet-address-delete-btn");
-	for(let i =0;i<x.length;i++){
-		x[i].addEventListener("click",function(){
-			let address = this.parentElement.getElementsByTagName("a")[0].innerText;
-			console.log(address,"Is to be deleted");
-		})
-	}
+(function() {
+	 $(".wallet-address-delete-btn").click(function(e) {
+		e.preventDefault();
+        let address = this.parentElement.getElementsByTagName("a")[0].innerText;
+        console.log(address);
+		$.ajax({
+			url : "/profile/settings/"+address,
+			method:"DELETE"
+		}).then(function(data){
+			window.location.href = "/profile/settings";
+		});
+	});
 })();
