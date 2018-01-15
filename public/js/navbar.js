2c8223aa485fb07c5970d8389a9c583ce5d12927
Admin <admin@Admins-MacBook-Pro.local>
2018-01-14 16:02:14 -0800

    able to switch between settings tabs

--- public/js/navbar.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("nav-tools-btn").addEventListener("click",function(){
-	console.log("tools btn was clicked");	
-})
-document.getElementById("nav-msg-btn").addEventListener("click",function(){
-	console.log("Message btn was clicked");	
-})
--- public/js/settings.js
+++ public/js/settings.js
@@ -10,13 +10,36 @@ function selectedWalletType(e) {
     }
     document.getElementById("walletTypeInput").value = e.innerText;
 }
-
+function selectedNavItem(e){
+    //removing acitve classes
+    document.getElementsByClassName("selected-settings-nav")[0].classList.remove("selected-settings-nav");
+    document.getElementsByClassName("active-settings-item")[0].classList.remove("active-settings-item");
+    //setting new ones
+    e.classList.add("selected-settings-nav");
+    console.log(e.innerText);
+    switch(e.innerText.toLowerCase()){
+        case "profile":
+            document.getElementById("profile-settings-wrapper").classList.add("active-settings-item");
+        break;
+        case "wallets":
+            document.getElementById("add-wallet-wrapper").classList.add("active-settings-item");
+        break;
+        default:
+            document.getElementById("profile-settings-wrapper").classList.add("active-settings-item");
+        break;
+    }
+}
 //IFFE
-
-//settings nav innertext when user visits settings page
-(function() {
-    document.getElementById("nav-name").innerText = "Settings";
+//event listeners for settings nav items
+(function(){
+    let x = document.getElementsByClassName("settings-nav-li");
+    for(let i=0;i<x.length;i++){
+        x[i].addEventListener("click", function() {
+            selectedNavItem(this);
+        })
+    }
 })();
+
 //adding event listeners for wallet type drop down items
 (function() {
     let x = document.getElementsByClassName("wallet-dropdown-item");
