491be9f2786b6c1d0a48d33d02a5d668e96937f1
taihelsel <tai.bayard@gmail.com>
2018-03-14 19:00:47 -0700

    updated nodemailer function

--- controllers/profile.js
+++ controllers/profile.js
@@ -92,10 +92,16 @@ var coinmarket = {
 
 function sendMessage(address, code) {
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_PASS
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
         }
     });
 
